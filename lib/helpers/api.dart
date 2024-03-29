8d41f582786ecbc7e886825c9e91d9097a577b99
Youssef <ykmhabib97@gmail.com>
2024-03-29 11:24:03 +0200

    Implemented put request in API class

--- lib/helpers/api.dart
+++ lib/helpers/api.dart
@@ -37,4 +37,32 @@ class Api {
           'There was a problem.\nStatus code:${response.statusCode} with body:${jsonDecode(response.body)}');
     }
   }
+
+  Future<dynamic> put({
+    required String url,
+    dynamic body,
+    String? token,
+  }) async {
+    Map<String, String> headers = {};
+    headers.addAll({
+      'Content-Type': 'application/x-www-form-urlencoded',
+    });
+
+    if (token != null) {
+      headers.addAll({'Authorization': 'Bearer $token'});
+    }
+    http.Response response = await http.post(
+      Uri.parse(url),
+      body: body,
+      headers: headers,
+    );
+
+    if (response.statusCode == 200) {
+      Map<String, dynamic> data = jsonDecode(response.body);
+      return data;
+    } else {
+      throw Exception(
+          'There was a problem.\nStatus code:${response.statusCode} with body:${jsonDecode(response.body)}');
+    }
+  }
 }
