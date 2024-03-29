0e9cad5bba18889a2e8547fc04a4d98bb053f12d
Youssef <ykmhabib97@gmail.com>
2024-03-29 14:37:46 +0200

    Finished product card

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class HomeView extends StatelessWidget {
@@ -19,50 +20,65 @@ class HomeView extends StatelessWidget {
         ],
       ),
       body: Center(
-        child: Container(
-          width: 225,
-          height: 125,
-          decoration: BoxDecoration(
-            boxShadow: [
-              BoxShadow(
-                blurRadius: 40,
-                color: Colors.grey.withOpacity(0.25),
-                spreadRadius: 0,
-                offset: const Offset(10, 10),
-              ),
-            ],
-          ),
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16),
-            child: Card(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.end,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const Text(
-                    'Handbag LV',
-                    style: TextStyle(color: Colors.grey),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      const Text(
-                        '\$254.99',
-                        style: TextStyle(color: Colors.grey),
-                      ),
-                      IconButton(
-                        onPressed: () {},
-                        icon: const Icon(
-                          Icons.favorite_outline,
-                          color: Colors.red,
-                        ),
-                      ),
-                    ],
+        child: Stack(
+          clipBehavior: Clip.none,
+          children: [
+            Container(
+              width: 225,
+              height: 125,
+              decoration: BoxDecoration(
+                boxShadow: [
+                  BoxShadow(
+                    blurRadius: 40,
+                    color: Colors.grey.withOpacity(0.25),
+                    spreadRadius: 0,
+                    offset: const Offset(10, 10),
                   ),
                 ],
               ),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                child: Card(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text(
+                          'Handbag LV',
+                          style: TextStyle(color: Colors.grey),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            const Text(
+                              '\$254.99',
+                            ),
+                            IconButton(
+                              onPressed: () {},
+                              icon: const Icon(
+                                Icons.favorite_outline,
+                                color: Colors.red,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
             ),
-          ),
+            Positioned(
+              right: 32,
+              bottom: 84,
+              child: Image.network(
+                'https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg',
+                height: 128,
+              ),
+            ),
+          ],
         ),
       ),
     );
